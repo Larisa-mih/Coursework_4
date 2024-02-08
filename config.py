ee36a23bebcd233494d008a6b1e756574e83f994
larisa <larisa.mih2004@gmail.com>
2024-02-09 01:15:07 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+ROOT_DIR = os.path.dirname(__file__)
+OPERATIONS_PATH = os.path.join(ROOT_DIR, "hh_vac.json")
+API_SJ = "v3.r.138144559.09c28c2dcad6ea2464f9c31b82bc8043279aabdb.6393106e24a46c61b719fe8fe0ba022479f79d14"
+HH_URL = "https://api.hh.ru/vacancies"
+SJ_URL = "https://api.superjob.ru/2.0/vacancies/"
